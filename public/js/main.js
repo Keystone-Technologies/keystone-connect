77ed04938de28c464201741562f6e146a2c0495c
Jason Lewis <thejasonslewis@gmail.com>
2015-04-28 00:20:17 -0600

    Adding app drawer and draggable icons and resizable columns
    really working on the integration of gridster, I have implemented
    columns at 90,90 margin 8px by 8px for the gaps added the app drawer at
    the bottom, working on multiple pages

--- public/js/main.js
+++ public/js/main.js
@@ -2,31 +2,37 @@
 
 $(function () {
 
-    $('.icon').resizable({
-        start: function(e, ui) {
-            console.log('resizing started');
-        },
-        resize: function(e, ui) {
+    //$('.icon').resizable({
+    //    start: function(e, ui) {
+    //        console.log('resizing started');
+    //    },
+    //    resize: function(e, ui) {
+    //
+    //    },
+    //    stop: function(e, ui) {
+    //        console.log('resizing stopped');
+    //    }
+    //});
+    //$(".icon").draggable({
+    //    containment: "#home",
+    //    scroll: false,
+    //    cursorAt: {
+    //        top: 15
+    //    },
+    //    axis: "y",
+    //    drag: function (event, ui) {
+    //        var margin = ui.offset.top
+    //
+    //    }
+    //});
+    //
+    //$("#home").droppable ({
+    //    drop: function() { alert('dropped'); }
+    //});
 
-        },
-        stop: function(e, ui) {
-            console.log('resizing stopped');
-        }
-    });
-    $(".icon").draggable({
-        containment: "#home",
-        scroll: false,
-        cursorAt: {
-            top: 15
-        },
-        axis: "y",
-        drag: function (event, ui) {
-            var margin = ui.offset.top
-
-        }
-    });
-
-    $("#home").droppable ({
-        drop: function() { alert('dropped'); }
-    });
+    $(".gridster ul").gridster({
+        widget_margins: [8, 8],
+        widget_base_dimensions: [90, 90],
+        min_cols: 8
+    })
 });
