8d60c7a383a9707880ba95af1939a8730c4776c2
Jason Lewis <thejasonslewis@gmail.com>
2015-04-29 20:11:49 -0600

    Working create folder and delete icon
    with this update the drag icon to the top triggers trash and the bottom
    triggers a folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 var gridster;
 
+$(".folder").click(function(){
+    $(".folder-modal").modal('show');
+});
+
 $(function () {
 
     //$('.icon').resizable({
@@ -13,28 +17,60 @@ $(function () {
     //        console.log('resizing stopped');
     //    }
     //});
-    //$(".icon").draggable({
-    //    containment: "#home",
-    //    scroll: false,
-    //    cursorAt: {
-    //        top: 15
-    //    },
-    //    axis: "y",
-    //    drag: function (event, ui) {
-    //        var margin = ui.offset.top
-    //
-    //    }
-    //});
-    //
-    //$("#home").droppable ({
-    //    drop: function() { alert('dropped'); }
-    //});
+    $(".gs-w").draggable({
+        containment: "#home",
+        scroll: false,
+        cursorAt: {
+            top: -15
+        },
+        axis: "y",
+
+        drag: function(event, ui) {
+            var margin = ui.offset.top;
+            if(margin<125){
+                //    $(this).parent().remove();
+                $("#trash").show();
+            }
+            else{
+                $("#trash").hide();
+            }
+            if(margin>$(window).height() - 100){
+                //    $(this).parent().remove();
+                $("#create-folder").show();
+            }
+            else{
+                $("#create-folder").hide();
+            }
+
+
+
+        }
+    });
+
+
+    $("#trash").droppable ({
+        tolerance: "touch",
+        drop: function(event, ui) {
+            $(ui.draggable).empty();
+            $(ui.draggable).append("<span class=\"gs-resize-handle gs-resize-handle-both\"></span>");
+            $("#trash").hide();
+        }
+    });
+    $("#create-folder").droppable ({
+        tolerance: "touch",
+        drop: function(event, ui) {
+            $(ui.draggable).empty();
+            $(ui.draggable).append("<div class=\"folder\"><h2>folder</h2></div>");
+            $(ui.draggable).append("<span class=\"gs-resize-handle gs-resize-handle-both\"></span>");
+            $("#create-folder").hide();
+        }
+    });
 
     gridster = $(".gridster ul").gridster({
         widget_margins: [8, 8],
         widget_base_dimensions: [90, 90],
         min_cols: 8,
-        helper: 'clone',
+        //helper: 'clone',
         resize: {
             enabled: true
         }
