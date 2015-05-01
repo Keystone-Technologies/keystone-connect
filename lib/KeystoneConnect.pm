5d488a71dfb97e31259be2fe9eab9b6bbb2e485b
Stefan Adams <Stefan.Adams@keystone-it.com>
2015-05-01 14:58:50 +0000

    KEYSTONE-28 Pseudo Backend API

--- lib/KeystoneConnect.pm
+++ lib/KeystoneConnect.pm
@@ -14,6 +14,48 @@ sub startup {
   # Normal route to controller
   #$r->get('/')->to('example#welcome');
   $r->get('/')->to(cb => sub { shift->redirect_to('/index.html') });
+
+  $r->get('/api/grid/:page')->to(cb => sub {
+    my $c = shift;
+    my $img = 'img/TwitterAppIcon.png';
+    my $label = 'twitter';
+    if ( $c->param('page') == 1 ) {
+      $img = 'img/FacebookAppIcon.png';
+      $label = 'facebook';
+    } elsif ( $c->param('page') == 2 ) {
+      $img = 'img/YouTubeAppIcon.png';
+      $label = 'youtube';
+    } elsif ( $c->param('page') == 3 ) {
+      $img = 'img/SpotifyAppIcon.png';
+      $label = 'spotify';
+    }
+    $c->render(json => [
+      {"col"=>1,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>3,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>2,"row"=>3,"size_x"=>2,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>3,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>5,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>4,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>6,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>1,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>9,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>7,"row"=>1,"size_x"=>2,"size_y"=>2,img=>$img,label=>$label},
+      {"col"=>6,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>7,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>9,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>4,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>6,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>5,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>1,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>5,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>4,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>2,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>8,"row"=>3,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>2,"row"=>1,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>9,"row"=>2,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label},
+      {"col"=>1,"row"=>4,"size_x"=>1,"size_y"=>1,img=>$img,label=>$label}
+    ]);
+  });
 }
 
 1;
