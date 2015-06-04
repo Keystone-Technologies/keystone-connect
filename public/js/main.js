46aff523a037071c1b028800c7066d892b48a056
Jason Lewis <thejasonslewis@gmail.com>
2015-06-04 09:57:24 -0600

    Keystone-57 activation hotspot and remove icon bug fixes, it was only happening once and then it wouldnt work, that is fixed now.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,361 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+163
+164
+165
+166
+167
+168
+169
+170
+171
+172
+173
+174
+175
+176
+177
+178
+179
+180
+181
+182
+183
+184
+185
+186
+187
+188
+189
+190
+191
+192
+193
+194
+195
+196
+197
+198
+199
+200
+201
+202
+203
+204
+205
+206
+207
+208
+209
+210
+211
+212
+213
+214
+215
+216
+217
+218
+219
+220
+221
+222
+223
+224
+225
+226
+227
+228
+229
+230
+231
+232
+233
+234
+235
+236
+237
+238
+239
+240
+241
+242
+243
+244
+245
+246
+247
+248
+249
+250
+251
+252
+253
+254
+255
+256
+257
+258
+259
+260
+261
+262
+263
+264
+265
+266
+267
+268
+269
+270
+271
+272
+273
+274
+275
+276
+277
+278
+279
+280
+281
+282
+283
+284
+285
+286
+287
+288
+289
+290
+291
+292
+293
+294
+295
+296
+297
+298
+299
+300
+301
+302
+303
+304
+305
+306
+307
+308
+309
+310
+311
+312
+313
+314
+315
+316
+317
+318
+319
+320
+321
+322
+323
+324
+325
+326
+327
+328
+329
+330
+331
+332
+333
+334
+335
+336
+337
+338
+339
+340
+341
+342
+343
+344
+345
+346
+347
+348
+349
+350
+351
+352
+353
+354
+355
+356
+357
+358
 var appTray;
 
 var gridster1;
@@ -11,34 +369,6 @@ var inputVal;
 var startingName;
 var $currentFolder;
 
-//var json =
-//    [
-//        { "size_y": 2, "row": 1, "col": 1, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com" },
-//        { "size_y": 2, "row": 2, "col": 3, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 2, "row": 3, "col": 2, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 2, "url": "http://facebook.com"  },
-//        { "size_y": 2, "row": 1, "col": 3, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 2, "row": 2, "col": 5, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 4, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 1, "col": 6, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 2, "row": 4, "col": 8, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 2, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 9, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 2, "row": 1, "col": 7, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 2, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 2, "col": 6, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 7, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 1, "col": 9, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 2, "col": 4, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 6, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 1, "col": 5, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 2, "col": 1, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 5, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 1, "col": 4, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 2, "col": 2, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 8, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 1, "col": 2, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 2, "col": 9, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  },
-//        { "size_y": 1, "row": 3, "col": 8, "label": "facebook", "img": "img\/FacebookAppIcon.png", "size_x": 1, "url": "http://facebook.com"  }
-//    ];
-
 function thumbnailDraggable($thumb) {
     $thumb.draggable({
         containment: "#home",
@@ -49,19 +379,17 @@ function thumbnailDraggable($thumb) {
         axis: "y",
         stop: function () {
             LockClickEvents($(this));
-            $("#trash").hide();
-            $("#create-folder").hide();
+//            $("#trash").hide();
+//            $("#create-folder").hide();
             $("#app-tray").droppable("destroy");
 
         },
         drag: function (event, ui) {
-
             appTrayDroppable();
             grid1Droppable();
-
             var margin = ui.offset.top;
-            $("#trash").show();
-            $("#create-folder").css("display", "block");
+//            $("#trash").show();
+//            $("#create-folder").css("display", "block");
         }
     });
 }
@@ -101,25 +429,23 @@ function LockClickEvents($object) {
     });
 }
 
-
-
-function appTrayDroppable($droppable,$accept) {
-    $droppable.droppable({
-        accept: $accept,
-
-        tolerance: "touch",
-        drop: function (event, ui) {
-            if($droppable.find("li").hasClass('thumb-empty')) {
-                var thumbCopy = $(ui.draggable).contents().clone();
-                $(ui.draggable).empty();
-                thumbCopy.appendTo($droppable.find("thumb-empty").first());
-                $droppable.find("thumb-empty").first()
-                    .removeClass("thumb-empty")
-                    .addClass("thumb-occupied");
-            }
-        }
-    });
-}
+//function appTrayDroppable($droppable,$accept) {
+//    $droppable.droppable({
+//        accept: $accept,
+//
+//        tolerance: "touch",
+//        drop: function (event, ui) {
+//            if($droppable.find("li").hasClass('thumb-empty')) {
+//                var thumbCopy = $(ui.draggable).contents().clone();
+//                $(ui.draggable).empty();
+//                thumbCopy.appendTo($droppable.find("thumb-empty").first());
+//                $droppable.find("thumb-empty").first()
+//                    .removeClass("thumb-empty")
+//                    .addClass("thumb-occupied");
+//            }
+//        }
+//    });
+//}
 
 function appTrayDroppable() {
     $("#app-tray").droppable({
@@ -164,14 +490,17 @@ function grid1Droppable() {
 
 function trashDroppable() {
     $("#trash").droppable({
-        tolerance: "pointer     ",
+        accept: ".gs-w",
+        hoverClass: "dropzone-hover",
+        activeClass: "dropzone-active",
+        tolerance: "pointer",
         drop: function (event, ui) {
             $(ui.draggable)
                 .empty()
                 .addClass("thumb-empty")
                 .removeClass("thumb-occupied");
 //            $(ui.draggable).append("<span class=\"gs-resize-handle gs-resize-handle-both\"></span>");
-            $("#trash").hide();
+//            $("#trash").hide();
         }
     });
 }
@@ -180,27 +509,22 @@ function createFolderDroppable() {
     console.log("drop function running");
     $("#create-folder").droppable({
         tolerance: "pointer",
-        hoverClass: ".dropzone-active",
+        hoverClass: "dropzone-hover",
+        activeClass: "dropzone-active",
         drop: function (event, ui) {
             $(ui.draggable).empty();
             $(ui.draggable)
-
-
                 .append("<div class=\"folder\"><h2>folder</h2></div>")
 //                .addClass("thumb-occupied")
                 .removeClass("thumb-empty");
             $(ui.draggable)
                 .append("<span class=\"gs-resize-handle gs-resize-handle-both\"></span>");
-            $("#create-folder").hide();
             showFolderModal();
         }
     });
 }
 
 function requestPassword() {
-//    var pass = $(".password-input").text();
-//    var user = $(".username-input").text();
-
     $("#password-modal").modal({
         backdrop : 'static',
         keyboard : false
@@ -213,9 +537,7 @@ function requestPassword() {
             console.log("modal closed");
             $("#password-modal").modal("hide");
         }
-
     });
-
 }
 
 function showFolderModal() {
