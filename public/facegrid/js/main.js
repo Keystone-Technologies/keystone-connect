9687b2af88393495ef6b2055772977f452111574
Jason Lewis <thejasonslewis@gmail.com>
2015-07-24 02:10:31 -0600

    facegrid scrolling on hover and adding icons above for tomorrow's discussion.

--- public/facegrid/js/main.js
+++ public/facegrid/js/main.js
@@ -13,27 +13,27 @@ var $currentFolder;
 var headFootHeight;
 var scrolltime = 200;
 var horizontalgridscroll = 100;
+var freewallWidth = $("#freewall1").css("width");
 
 
-
-var tempJSON = [
-    {"width":100, "height":100, "src":"temp/images-1.jpg", "class": "", "text":"Mom", "email":"ktravis@connexinsoftware.com "},
-    {"width":100, "height":100, "src":"temp/images-1.jpg", "class": "", "text":"Dad", "email":"stravis@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/images-2.jpg", "class": "", "text":"Andy", "email":"abelval@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/images-3.jpg", "class": "", "text":"Stefan", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/images-4.jpg", "class": "", "text":"Mason", "email":"mdrothert@gmail.com"},
-    {"width":100, "height":100, "src":"temp/images-5.jpg", "class": "", "text":"Jason", "email":"thejasonslewis@gmail.com"},
-    {"width":100, "height":100, "src":"temp/images-6.jpg", "class": "", "text":"Ben", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/images-7.jpg", "class": "", "text":"Rob", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-12.jpg", "class": "", "text":"Eric", "email":"ehumes@keystone-it.com"},
-    {"width":100, "height":100, "src":"temp/images-8.jpg", "class": "", "text":"Caleb", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/images-9.jpg", "class": "", "text":"Adam", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-10.jpg", "class": "", "text":"Montez", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Dave", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Cody", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Josh", "email":"sadams@keystone-technologies.com"},
-    {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Keith", "email":"sadams@keystone-technologies.com"}
-];
+    var tempJSON = [
+        {"width":100, "height":100, "src":"temp/images-1.jpg", "class": "", "text":"Mom", "email":"ktravis@connexinsoftware.com "},
+        {"width":100, "height":100, "src":"temp/images-1.jpg", "class": "", "text":"Dad", "email":"stravis@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/images-2.jpg", "class": "", "text":"Andy", "email":"abelval@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/images-3.jpg", "class": "", "text":"Stefan", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/images-4.jpg", "class": "", "text":"Mason", "email":"mdrothert@gmail.com"},
+        {"width":100, "height":100, "src":"temp/images-5.jpg", "class": "", "text":"Jason", "email":"thejasonslewis@gmail.com"},
+        {"width":100, "height":100, "src":"temp/images-6.jpg", "class": "", "text":"Ben", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/images-7.jpg", "class": "", "text":"Rob", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-12.jpg", "class": "", "text":"Eric", "email":"ehumes@keystone-it.com"},
+        {"width":100, "height":100, "src":"temp/images-8.jpg", "class": "", "text":"Caleb", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/images-9.jpg", "class": "", "text":"Adam", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-10.jpg", "class": "", "text":"Montez", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Dave", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Cody", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Josh", "email":"sadams@keystone-technologies.com"},
+        {"width":100, "height":100, "src":"temp/imgres-11.jpg", "class": "", "text":"Keith", "email":"sadams@keystone-technologies.com"}
+    ];
 
 function freewallAddCells(){
     var temp =
@@ -67,7 +67,7 @@ function addMenuToIcons() {
     $(".icon-menu ul").append(deleteItem);
     $(".icon-menu ul").append(addToItem);
     populateAddTo();
-
+    
 }
 
 function populateAddTo() {
@@ -77,7 +77,7 @@ function populateAddTo() {
         $(".add-flyout").append("<li class=\"add-flyout-item list-unstyled\">" + $this + "</li>");
     });
     deleteItem();
-}
+} 
 
 
 function deleteItem(){
@@ -115,37 +115,63 @@ function freewallInit() {
 //            $(".folder").removeClass("brick");
         }
     });
-
+    
     wall.fitWidth();
     console.log("grids loaded");
 }
+
 function sizeVideoFrame(){
     headFootHeight =  $("#header").outerHeight() + $(".grid-container").outerHeight();
     var iframeHeight = document.documentElement.clientHeight - (headFootHeight - 30);
     $("#vid-container iframe").css("height", iframeHeight)
 };
-
+  
 function staticEventHandlers() {
     $(window).resize(function() {
+        $("#freewall1").unwrap().unwrap();
+        addHoverScroll();
         sizeVideoFrame();
+    });
+
+}
+
+function swipeHandlers(){
+    $(".grid-container").on('swipeleft', function(){
+       $(this).animate({scrollLeft: $(this).scrollLeft() + horizontalgridscroll}, scrolltime);
+    });
+    $(".grid-container").on('swiperight', function(){
+       $(this).animate({scrollLeft: $(this).scrollLeft() - horizontalgridscroll}, scrolltime);
     });
 }
 
-
-function swipeHandlers(){
-
-    $(".grid-container").on('swipeleft', function(){
-        $(this).animate({scrollLeft: $(this).scrollLeft() + horizontalgridscroll}, scrolltime);
-    });
-    $(".grid-container").on('swiperight', function(){
-        $(this).animate({scrollLeft: $(this).scrollLeft() - horizontalgridscroll}, scrolltime);
-    });
+function addHoverScroll(){
+    $('#freewall1').hoverscroll(
+        {
+        	vertical: false,	// Display the list vertically or horizontally
+        	width:    "100%",		// Width of the list container
+        	height:   126,		// Height of the list container
+        	arrows:   true,		// Display direction indicator arrows or not
+        	arrowsOpacity: 0.7,	// Max possible opacity of the arrows
+            fixedArrows: false,  // Fixed arrows on the sides of the list (disables arrowsOpacity)
+            rtl:      true,     // Print images from right to left
+        	debug:    false,     // Debug output in the firebug console
+        }
+    ).css("height","126px").css("width",freewallWidth);
+    $(".grid-container").css("width","100%");
 }
 
 
 $(function () {
     freewallAddCells();
     freewallInit();
-    swipeHandlers();
+    // swipeHandlers();
     staticEventHandlers();
+    addHoverScroll();
+    $("#left-full").click(function () {
+        $('.grid-container').animate({ scrollLeft: '+=-' + horizontalgridscroll }, scrolltime, 'easeOutQuad');
+        console.log('clicked');
+    });
+    $("#right-full").click(function () {
+        $('.grid-container').animate({ scrollLeft: '+=' + horizontalgridscroll}, scrolltime, 'easeOutQuad');
+    });
 });
